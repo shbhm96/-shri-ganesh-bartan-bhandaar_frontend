99724552fc329b98ead4fdadf74aa7c5a7ea3bc2
shbhm96 <shbhm96@gmail.com>
2023-06-20 22:47:11 +0530

    add headre

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer>
       <Container style={{"background":"black"}}>
         <Row>
-          <Col className="text-center py-3">
-            
+          <Col className="text-center py-3" style={{"color":"white"}}>
+              <Link to="/home">Home</Link>
+              <Link to="/about-us">About Us</Link>
+              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-youtube"></i>
+              <i class="fa-brands fa-twitter"></i>
+              
           </Col>
         </Row>
       </Container>
