a97313a1a453ec480ac6f2ed5039a8f9bc6954a1
shbhm96 <shbhm96@gmail.com>
2023-08-27 08:41:06 +0530

    correcting loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,7 @@ import { Spinner } from 'react-bootstrap'
 const Loader = () => {
   return (
     <Spinner 
-      animation='borber' 
+      animation='border' 
       role="status" 
       style={{
         width:"100px",
